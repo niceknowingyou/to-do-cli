54ce5886269dc3fb6e84e870b4d02833cf7d7f0e
niceknowingyou <102244809+niceknowingyou@users.noreply.github.com>
2026-04-15 13:51:03 +0200

    reading a .txt files content

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import fs from 'node:fs';
+
+fs.readFile('./list.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
