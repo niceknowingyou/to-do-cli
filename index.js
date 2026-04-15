c18bc7d3b4228388877c90313312e99c0c5034e0
niceknowingyou <102244809+niceknowingyou@users.noreply.github.com>
2026-04-15 17:02:05 +0200

    appendFile function added

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import fs from 'node:fs';
 
+const content = "content";
+
+fs.appendFile('./list.txt', content, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("content added");
+  }
+})
+
 fs.readFile('./list.txt', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
