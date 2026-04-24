5ab6bc07e3604389d78937b612ba92dafabd805b
niceknowingyou <102244809+niceknowingyou@users.noreply.github.com>
2026-04-24 14:29:09 +0200

    Add menu module WIP

---
+++ menu.js
@@ -0,0 +1,20 @@
+export function menu (input, path) {
+if (input === undefined) {
+  rl.question("[a]dd, [d]el or cancel [any key]? \n", (answer) => {
+    if (answer === "a") {
+      rl.question("what do you want to add? \n", (content) => {
+        addToList(path, content);
+        rl.close();
+      });
+    } else if (answer === "d") {
+      fs.writeFile(path, '', function(){
+        console.log('\x1b[32m', "file cleared", '\x1b[0m');
+      });
+      rl.close();
+    } else {
+      console.log("nothing added or removed try running program again.");
+      rl.close();
+    }
+  });
+
+}
