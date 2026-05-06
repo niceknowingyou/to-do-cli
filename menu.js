f5e5a177387d513f7ca45a78a7c467243798d323
niceknowingyou <102244809+niceknowingyou@users.noreply.github.com>
2026-05-06 11:54:50 +0200

    Fix error when adding through menu

--- menu.js
+++ menu.js
@@ -6,19 +6,19 @@ export function menu (rl, path) {
   rl.question("[a]dd, [d]el or cancel [any key]? \n", (answer) => {
     if (answer === "a") {
       rl.question("what do you want to add? \n", (content) => {
-        let data = fs.readFileSync("list.json");
+        let data = fs.readFileSync(path);
         let myJsonList = JSON.parse(data);
         myJsonList.list.push(content);
         let updatedJson = JSON.stringify(myJsonList);
-        addToJson("list.json", updatedJson);
+        addToJson(path, updatedJson);
       });
     } else if (answer === "d") {
       rl.question("what item(number) do you want to delete? \n", (number) => {
-        let data = fs.readFileSync("list.json");
+        let data = fs.readFileSync(path);
         let myJsonList = JSON.parse(data);
         myJsonList.list.splice(number,1);
         let updatedJson = JSON.stringify(myJsonList);
-        addToJson("list.json", updatedJson);
+        addToJson(path, updatedJson);
       });
     } else {
       console.log("nothing added or removed try running program again.");
